543fe52e136ab689c2f763226ff302f2ba0468f5
tanjoshua <jtanjoshua@gmail.com>
2022-08-22 21:19:56 -0700

    more appointment model defs

--- server/src/booking/entities/Appointment.ts
+++ server/src/booking/entities/Appointment.ts
@@ -1,4 +1,4 @@
-import { Entity, ManyToOne, OneToOne, Property } from "@mikro-orm/core";
+import { Entity, Property } from "@mikro-orm/core";
 import { User } from "../../base/entities";
 import { BaseEntity } from "../../base/entities/BaseEntity";
 import { Schedule } from "./Schedule";
@@ -16,4 +16,37 @@ export class Appointment extends BaseEntity {
 
   @Property()
   endTime: Date;
+}
+
+@Entity()
+export class PendingAppointment extends BaseEntity {
+  @Property()
+  owner: User;
+
+  @Property()
+  schedule: Schedule;
+
+  @Property()
+  startTime: Date;
+
+  @Property()
+  endTime: Date;
+
+  @Property()
+  appointment: Appointment;
+}
+
+@Entity()
+export class AppointmentClient extends BaseEntity {
+  @Property()
+  user: User; // this is optional - probably not used in early versions
+
+  @Property()
+  name: string;
+
+  @Property()
+  phoneNumber: string;
+
+  @Property()
+  note: string;
 }
