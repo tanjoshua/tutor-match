6460274d8b8c24e7f5a41c79af40d5ebe75315e7
tanjoshua <jtanjoshua@gmail.com>
2023-03-10 11:28:40 -0500

    formatted options

--- server/src/tutor/models/Profile.ts
+++ server/src/tutor/models/Profile.ts
@@ -5,7 +5,7 @@ export enum Level {
   // Primary
   Pri1 = "Primary 1",
   Pri2 = "Primary 2",
-  Pr3 = "Primary 3",
+  Pri3 = "Primary 3",
   Pri4 = "Primary 4",
   Pri5 = "Primary 5",
   Pri6 = "Primary 6",
@@ -20,19 +20,19 @@ export enum Level {
   Sec2Express = "Secondary 2 (Express)",
   Sec2NA = "Secondary 2 (Normal Academic)",
   Sec2NT = "Secondary 2 (Normal Technical)",
-  Sec2IP = "Secondary 2 (Integrated Program",
+  Sec2IP = "Secondary 2 (Integrated Program)",
 
   // Sec 3
   Sec3Express = "Secondary 3 (Express)",
   Sec3NA = "Secondary 3 (Normal Academic)",
   Sec3NT = "Secondary 3 (Normal Technical)",
-  Sec3IP = "Secondary 3 (Integrated Program",
+  Sec3IP = "Secondary 3 (Integrated Program)",
 
   // Sec 4
   Sec4Express = "Secondary 4 (Express)",
   Sec4NA = "Secondary 4 (Normal Academic)",
   Sec4NT = "Secondary 4 (Normal Technical)",
-  Sec4IP = "Secondary 4 (Integrated Program",
+  Sec4IP = "Secondary 4 (Integrated Program)",
 
   // JC
   JC1 = "Junior College 1 (JC1)",
