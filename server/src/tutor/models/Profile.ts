1103e1db5f170e91f343212e94cafb6adfb5d9ef
tanjoshua <jtanjoshua@gmail.com>
2023-03-16 17:00:21 -0400

    other level and subjects

--- server/src/tutor/models/Profile.ts
+++ server/src/tutor/models/Profile.ts
@@ -77,6 +77,7 @@ export default class TutorProfile {
     lowerSecondary: string[];
     upperSecondary: string[];
     jc: string[];
+    other: string[];
   };
   public type: TutorType; // eg. full time, part time, etc
   public qualifications: string; // for teaching experience and academic
